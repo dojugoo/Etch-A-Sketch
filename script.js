d8d638b35bbe84db6b368271d7fc68c4e52aae2c
ulandlau <ulandlau@gmail.com>
2022-09-02 21:52:06 -0700

    fixed flexbox container to better display grid

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 const container = document.querySelector("#container");
 
 //Nested for loops to create grid of div squares
-for (let i = 0; i < 25; i++) {
+for (let i = 0; i < 20; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
     container.appendChild(row);
-    for (let j = 0; j < 25; j++) {
+    for (let j = 0; j < 20; j++) {
         const box = document.createElement("div");
         box.classList.add("box");
         row.appendChild(box);
