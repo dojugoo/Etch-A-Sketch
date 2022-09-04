a0855b865a06b16a49a0d0ce0b2568dfe5b8cbfe
ulandlau <ulandlau@gmail.com>
2022-09-03 19:15:18 -0700

    Added button function with user input value to create new grid

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
 //Making square grid using flexbox
 const container = document.querySelector("#container");
 
+//Constants
+let gridsize = 20;
+createGrid(gridsize);
+boxes();
+
+//Prompt button for grid size
+const btn = document.querySelector("#grid");
+btn.addEventListener("click", gridFunction);
+
+//Clears grid, remake grid and restart script
+function gridFunction() {
+    let a = prompt("Enter value for grid size up to 100:");
+    while (a > 100) {
+        a = prompt("Enter value for grid size up to 100:");
+    }
+    gridsize = a;
+    //Clears grid within container
+    const removal = document.querySelector("#container");
+    removal.textContent = '';
+    createGrid(gridsize); //creates new grid with user specified number
+    boxes();
+}
+
 //Nested for loops to create grid of div squares
-for (let i = 0; i < 20; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    for (let j = 0; j < 20; j++) {
-        const box = document.createElement("div");
-        box.classList.add("box");
-        row.appendChild(box);
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for (let j = 0; j < size; j++) {
+            const box = document.createElement("div");
+            box.classList.add("box");
+            row.appendChild(box);
+        }
     }
 }
 
 //Add event listener to mouse hover for all boxes
-let boxes = document.querySelectorAll(".box");
-boxes.forEach(function(value) {
-    value.addEventListener("mouseenter", changeBox);
-});
+function boxes() {
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach(function(value) {
+        value.addEventListener("mouseenter", changeBox);
+    });
+}
 
 //Adds alteredbox class to hovered boxes
 function changeBox(e) {
